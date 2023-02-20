d8e20bcf7458751c20dcef4e3e145e2d3c87e2b8
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2023-02-21 00:13:07 +0100

    db password from Secret

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import json
 import mariadb
 import os
 import sys
+import kubernetes.client
+import kubernetes.config
+from openshift.dynamic import DynamicClient
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -41,13 +44,28 @@ def getQuoteById(id):
 
 @app.route('/quotes/random', methods=['GET'])
 def getRandom():
+    quotes = []
     try:
+        kubernetes.config.load_incluster_config();
+        
+        #kubernetes.config.load_kube_config()  
+        k8s_client = kubernetes.config.new_client_from_config()
+        dyn_client = DynamicClient(k8s_client)
+
+        k8s_secrets = dyn_client.resources.get(api_version='v1', kind='Secret')
+
+        pwd = k8s_secrets.get(name='mysqlpassword', namespace='daszkiewiczk-dev')
+
+        print(pwd)
+
+        #pwd = kubernetes.client.CoreV1Api().read_namespaced_secret("mysqlpassword").data
         conn = mariadb.connect(
         user="root",
-        password="admin",
+        password = pwd,
         host=os.environ.get('DB_SERVICE_NAME'),
         database="quotesdb",
         port=3306)
+
         db_cursor = conn.cursor(dictionary=True)
         db_cursor.execute("SELECT '-hostname-' as hostname, id, quotation, author FROM quotes ORDER BY author, id") 
         quotes = mycursor.fetchall()
@@ -59,10 +77,6 @@ def getRandom():
     except mariadb.Error as e:
         print("Error connecting to db")
         sys.exit(1)
-
-
-
-    return prepareResponse(jsonify(replaceHostname(quotes[n])))
 
 def prepareResponse(response):
     # Enable Access-Control-Allow-Origin
